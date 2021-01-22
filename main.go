c70d8471c7e7f11c827bcbf3989ccae148c42cc8
levi.l <levi840714@gmail.com>
2021-01-22 14:26:54 +0800

    feat: run sell cro flow

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
+	"SellAllCro/cdc"
 	"SellAllCro/config"
 	"fmt"
 	"log"
-)
-
-const (
-	ProdUri = "https://api.crypto.com/v2/"
+	"time"
 )
 
 func init() {
@@ -18,5 +16,29 @@ func init() {
 }
 
 func main() {
-	fmt.Println("SellAllCro!!!")
+	log.Println("Start sell all CRO automatically on crypto.com exchange")
+
+	for {
+		balance, err := cdc.GetCroBalance()
+		if err != nil {
+			log.Printf("Get account CRO balance failed, err: %s", err)
+			continue
+		}
+
+		orderId, err := cdc.CreateOrder(balance)
+		if err != nil {
+			log.Printf("Create sell CRO order failed, err: %s", err)
+		}
+
+		order, err := cdc.GetOrderDetail(orderId)
+		if err != nil {
+			log.Printf("Get order detail failed, err: %s", err)
+		}
+
+		if order.Code == 0 && order.Result.OrderInfo.Status == "FILLED" {
+			fmt.Printf("Sell %v CRO to %v %s success!!", order.Result.OrderInfo.CumulativeQuantity, order.Result.OrderInfo.CumulativeValue, config.Config.ToCoin)
+		}
+
+		time.Sleep(time.Hour * 1)
+	}
 }
